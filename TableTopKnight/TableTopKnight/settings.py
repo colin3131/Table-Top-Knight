6a0e5130b292719874faf3928640bcc09294b38a
Colin Spratt <cps38@pitt.edu>
2019-11-01 18:19:47 -0400

    Got db working, added added to models

--- TableTopKnight/TableTopKnight/settings.py
+++ TableTopKnight/TableTopKnight/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'TableTopKnight.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TTK',
+        'USER': 'TTKAuth',
+        'PASSWORD': 'Group%20',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- TableTopKnight/database/models.py
+++ TableTopKnight/database/models.py
@@ -1,14 +1,43 @@
 from django.db import models
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
 
 # User Table
 class Profile(models.Model):
-    user = models.OneToOneField('User', on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField(max_length=100)
     friends = models.ManyToManyField('self')
-    notifications = models.CharField(max_length=None)
-    library = models.ManyToManyField(Game)
+    notifications = ArrayField(
+        JSONField()
+    )
+    library = models.ManyToManyField("Game")
+
+    def __str__(self):
+        return self.user.username
+
+    def verifyLogin(self, _username, _password):
+        _user = authenticate(_username, _password)
+        if _user is not None:
+            return True
+        else:
+            return False
+
+    def changePassword(self, oldPass, newPass):
+        _user = authenticate(self.user.username, oldPass)
+        if _user is not None:
+            self.user.set_password(newPass)
+            return True
+        else:
+            return False
+    
+    def getFriends(self):
+        return self.friends.all()
+        
 
 # Game Table
 class Game(models.Model):
@@ -22,7 +51,6 @@ class Game(models.Model):
 # Event Table
 class Event(models.Model):
     host = models.ForeignKey('Profile', on_delete=models.CASCADE)
-    attendees = models.ManyToManyField("Profile")
+    attendees = models.ManyToManyField("Profile", related_name="event_hosting")
     eventDateTime = models.DateTimeField(auto_now=False, auto_now_add=False)
     eventGames = models.ManyToManyField("Game")
-
