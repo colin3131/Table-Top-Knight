45ef65ea742785fe8fe4986c03b8e5b64b868cfa
Aliyah Flores <aff13@pitt.edu>
2019-11-20 00:01:05 -0500

    Tried to fix bugs

--- TableTopKnight/database/tests.py
+++ TableTopKnight/database/tests.py
@@ -22,70 +22,70 @@ class ProfileTest(TestCase):
 
 	def verifyLogin(self): 	
 		# Returns True or False
-		colin = User.objects.get(username="colin", email="colin@gmail.com", password="testpass123")
+		colin = User.objects.get(username="colin")
 		self.assertTrue(colin.profile.verifyLogin("colin", "testpass123"))
 
 	def changePassword(self):
 		# Returns True or False (based on success)
-		colin = User.objects.get(username="colin", email="colin@gmail.com", password="testpass123")
+		colin = User.objects.get(username="colin")
 		colin.profile.set_password(password="newpass123")
 		self.assertTrue(colin.profile.changePassword("testpass123", "newpass123"))
 
 	def addFriend(self):
 		# Returns True or False (based on success)
-		connor = User.objects.get(username="connor", email="connor@gmail.com", password="testpass123")
+		connor = User.objects.get(username="connor")
 		colin = User.objects.get(username="colin", email="colin@gmail.com", password="testpass123")
 		self.assertTrue(connor.profile.addFriend(colin))
 
 	def removeFriend(self):
 		# Returns True or False (based on success)
-		connor = User.objects.get(username="connor", email="connor@gmail.com", password="testpass123")
+		connor = User.objects.get(username="connor")
 		colin = User.objects.get(username="colin", email="colin@gmail.com", password="testpass123")
 		self.assertTrue(connor.profile.removeFriend(colin))
 	
 	def getLibrary(self):
 		# Returns a list of owned games
 		pokemonGO = Game.objects.get(gameName="pokemonGO")
-		jackson = User.objects.get(username="jackson", email="jackson@gmail.com", password="testpass123")
+		jackson = User.objects.get(username="jackson")
 		jackson.addGame(pokemonGO)
 		self.assertEqual(pokemonGO, jackson.getLibrary().get(pk=pokemonGO.ID))
 
 	def addGame(self):
 		# Returns True or False (based on success)
 		pokemonGO = Game.objects.get(gameName="pokemonGO")
-		jackson = User.objects.get(username="jackson", email="jackson@gmail.com", password="testpass123")
+		jackson = User.objects.get(username="jackson")
 		self.assertTrue(jackson.addGame(pokemonGO))
 
 	def removeGame(self):
 		# Returns True or False (based on success)
 		pokemonGO = Game.objects.get(gameName="pokemonGO")
-		jackson = User.objects.get(username="jackson", email="jackson@gmail.com", password="testpass123")
+		jackson = User.objects.get(username="jackson")
 		self.assertTrue(jackson.removeGame(pokemonGO))
 
 	def getNotifications(self):
 		# Returns a list of all notifications
-		connor = User.objects.get(username="connor", email="connor@gmail.com", password="testpass123")
+		connor = User.objects.get(username="connor")
 		connor.addNotification("You've been added to a game!")
 		self.assertTrue(connor.getNotifications())
 
 	def addNotification(self):
 		# Creates a notification for a user
-		connor = User.objects.get(username="connor", email="connor@gmail.com", password="testpass123")
+		connor = User.objects.get(username="connor")
 		self.assertTrue(connor.addNotification("You've been added to a game", "https://google.com"))
 
 	def removeNotification(self):
 		# Returns True or False (based on success)
-		connor = User.objects.get(username="connor", email="connor@gmail.com", password="testpass123")
+		connor = User.objects.get(username="connor")
 		self.assertTrue(connor.removeNotification("You've been added to a game"))
 
 	def getEventsHosting(self):
 		# Returns a list of events a user is hosting
-		jackson = User.objects.get(username="jackson", email="jackson@gmail.com", password="testpass123")
+		jackson = User.objects.get(username="jackson")
 		self.assertTrue(jackson.getEventsHosting())
 
 	def getEventsAttending(self):
 		# Returns a list of events a user is attending
-		jackson = User.objects.get(username="jackson", email="jackson@gmail.com", password="testpass123")
+		jackson = User.objects.get(username="jackson")
 		self.assertTrue(jackson.getEventsAttending())
 
 # Event Model
@@ -173,13 +173,13 @@ class EventTest(TestCase):
 		self.assertEqual(event.event_state, event.AFTER_VOTING)
 
 	def	getFilteredGames(self):
+		# Returns a list of games that users own, filtered by the amount of players
 		game1 = Game.objects.create_game(gameName="PokemonGo", playerMin=1, playerMax=10, genre="RPG", thmb="pkmn", desc="It's a game")
 		game2 = Game.objects.create_game(gameName="The Legend of Zelda", playerMin=1, playerMax=1, genre="FirstPerson", thmb="zelda", desc="It's a game")
 		game3 = Game.objects.create_game(gameName="Call of Duty", playerMin=1, playerMax=8, genre="FPS", thmb="cod", desc="It's a game")
 		gameLibrary = [game1, game2, game3]
 		correctOrder = [game2, game3, game1]
 		self.assertTrue(gameLibrary[correctOrder])
-		# Returns a list of games that users own, filtered by the amount of players
 		
 	def getRankedGames(self):
 		# Returns a list of games that have been chosen based on the voting phase
